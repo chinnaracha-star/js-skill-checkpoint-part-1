1dd437ccf1e0a5441e508947a42f6ad742b3f34e
ChinDurex <chinnaracha@gmail.com>
2026-09-11 15:14:00 +0700

    Update question-2.js

--- question-2.js
+++ question-2.js
@@ -9,7 +9,7 @@ inventory.push({ name: "Orange", price: 20, quantity: 300 });
 
 let totals = 0;
 for (let i of inventory) {
-  totals = i.price * i.quantity;
+  totals += i.price * i.quantity;
   console.log(totals);
 }
 
